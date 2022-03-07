17c13c4e331f9579e9114694417909b8c3919f64
davidtrom <dtrombello@gmail.com>
2022-03-07 17:25:25 -0500

    updated folder structure for organization in project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from './Components/login/login.component';
+import { LoginComponent } from './Components/User/login/login.component';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './Components/about/about.component';
-import { PersonalRecordsComponent } from './Components/personal-records/personal-records.component';
-import { ContactComponent } from './Components/contact/contact.component';
-import { SupplyListComponent } from './Components/supply-list/supply-list.component';
+import { AboutComponent } from './Components/General/about/about.component';
+import { PersonalRecordsComponent } from './Components/Results/personal-records/personal-records.component';
+import { ContactComponent } from './Components/General/contact/contact.component';
+import { SupplyListComponent } from './Components/SupplyLists/supply-list/supply-list.component';
 import { UpcomingRacesComponent } from './Components/upcoming-races/upcoming-races.component';
-import { RaceShoesComponent } from './Components/race-shoes/race-shoes.component';
-import { RegisterComponent } from './Components/register/register.component';
-import { ForgotPasswordComponent } from './Components/forgot-password/forgot-password.component';
-import { ForgotUsernameComponent } from './Components/forgot-username/forgot-username.component';
-import { CreateUserComponent } from './Components/create-user/create-user.component';
-import { ProfileComponent } from './Components/profile/profile.component';
-import { CreateSupplyListComponent } from './Components/create-supply-list/create-supply-list.component';
-import { EditProfileComponent } from './Components/edit-profile/edit-profile.component';
-import { ViewSupplyListComponent } from './Components/view-supply-list/view-supply-list.component';
-import { EditListComponent } from './Components/edit-list/edit-list.component';
-import { RaceResultsComponent } from './Components/race-results/race-results.component';
-import { RaceShoeDetailComponent } from './Components/race-shoe-detail/race-shoe-detail.component';
-import { AddRaceShoeComponent } from './Components/add-race-shoe/add-race-shoe.component';
-import { ViewListNamesComponent } from './Components/view-list-names/view-list-names.component';
+import { RaceShoesComponent } from './Components/Shoes/race-shoes/race-shoes.component';
+import { ForgotPasswordComponent } from './Components/User/forgot-password/forgot-password.component';
+import { ForgotUsernameComponent } from './Components/User/forgot-username/forgot-username.component';
+import { CreateUserComponent } from './Components/User/create-user/create-user.component';
+import { ProfileComponent } from './Components/User/profile/profile.component';
+import { CreateSupplyListComponent } from './Components/SupplyLists/create-supply-list/create-supply-list.component';
+import { EditProfileComponent } from './Components/User/edit-profile/edit-profile.component';
+import { ViewSupplyListComponent } from './Components/SupplyLists/view-supply-list/view-supply-list.component';
+import { EditListComponent } from './Components/SupplyLists/edit-list/edit-list.component';
+import { RaceResultsComponent } from './Components/Results/race-results/race-results.component';
+import { RaceShoeDetailComponent } from './Components/Shoes/race-shoe-detail/race-shoe-detail.component';
+import { AddRaceShoeComponent } from './Components/Shoes/add-race-shoe/add-race-shoe.component';
+import { ViewListNamesComponent } from './Components/SupplyLists/view-list-names/view-list-names.component';
+import { SidebarNavComponent } from './Components/sidebar-nav/sidebar-nav.component';
 // import { ViewProfileComponent } from './Components/view-profile/view-profile.component';
 
 const routes: Routes = [
@@ -33,7 +33,7 @@ const routes: Routes = [
   {path: 'race-shoes', component: RaceShoesComponent},
   {path: 'shoe-detail/:shoeId', component: RaceShoeDetailComponent},
   {path: 'add-shoe', component: AddRaceShoeComponent},
-  {path: 'register', component: RegisterComponent},
+  // {path: 'register', component: RegisterComponent},
   {path: 'forgot-password', component: ForgotPasswordComponent},
   {path: 'forgot-username', component: ForgotUsernameComponent},
   {path: 'new-user', component: CreateUserComponent},
@@ -44,6 +44,7 @@ const routes: Routes = [
   {path: 'edit-list', component: EditListComponent},
   {path: 'race-results/:userId', component: RaceResultsComponent},
   {path: 'view-lists', component: ViewListNamesComponent},
+  {path: 'sidebar', component: SidebarNavComponent},
     
 
   // if no route, redirect to home
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,34 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AboutComponent } from './Components/about/about.component';
-import { LoginComponent } from './Components/login/login.component';
+import { AboutComponent } from './Components/General/about/about.component';
+import { LoginComponent } from './Components/User/login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
-import { HeaderComponent } from './Components/header/header.component';
-import { FooterComponent } from './Components/footer/footer.component';
-import { PersonalRecordsComponent } from './Components/personal-records/personal-records.component';
+import { HeaderComponent } from './Components/General/header/header.component';
+import { FooterComponent } from './Components/General/footer/footer.component';
+import { PersonalRecordsComponent } from './Components/Results/personal-records/personal-records.component';
 import { UpcomingRacesComponent } from './Components/upcoming-races/upcoming-races.component';
-import { SupplyListComponent } from './Components/supply-list/supply-list.component';
-import { ContactComponent } from './Components/contact/contact.component';
-import { RegisterComponent } from './Components/register/register.component';
-import { ForgotPasswordComponent } from './Components/forgot-password/forgot-password.component';
-import { ForgotUsernameComponent } from './Components/forgot-username/forgot-username.component';
+import { SupplyListComponent } from './Components/SupplyLists/supply-list/supply-list.component';
+import { ContactComponent } from './Components/General/contact/contact.component';
+import { ForgotPasswordComponent } from './Components/User/forgot-password/forgot-password.component';
+import { ForgotUsernameComponent } from './Components/User/forgot-username/forgot-username.component';
 import { SidebarNavComponent } from './Components/sidebar-nav/sidebar-nav.component';
-import { CreateUserComponent } from './Components/create-user/create-user.component';
-import { ProfileComponent } from './Components/profile/profile.component';
-import { CreateSupplyListComponent } from './Components/create-supply-list/create-supply-list.component';
-import { EditProfileComponent } from './Components/edit-profile/edit-profile.component';
-import { ViewSupplyListComponent } from './Components/view-supply-list/view-supply-list.component';
-import { EditListComponent } from './Components/edit-list/edit-list.component';
+import { CreateUserComponent } from './Components/User/create-user/create-user.component';
+import { ProfileComponent } from './Components/User/profile/profile.component';
+import { CreateSupplyListComponent } from './Components/SupplyLists/create-supply-list/create-supply-list.component';
+import { EditProfileComponent } from './Components/User/edit-profile/edit-profile.component';
+import { ViewSupplyListComponent } from './Components/SupplyLists/view-supply-list/view-supply-list.component';
+import { EditListComponent } from './Components/SupplyLists/edit-list/edit-list.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { RaceResultsComponent } from './Components/race-results/race-results.component';
-import { AddRaceShoeComponent } from './Components/add-race-shoe/add-race-shoe.component';
-import { RaceShoeDetailComponent } from './Components/race-shoe-detail/race-shoe-detail.component';
-import { RaceShoesComponent } from './Components/race-shoes/race-shoes.component';
-import { ViewProfileComponent } from './Components/view-profile/view-profile.component';
-import { ViewListNamesComponent } from './Components/view-list-names/view-list-names.component';
+import { RaceResultsComponent } from './Components/Results/race-results/race-results.component';
+import { AddRaceShoeComponent } from './Components/Shoes/add-race-shoe/add-race-shoe.component';
+import { RaceShoeDetailComponent } from './Components/Shoes/race-shoe-detail/race-shoe-detail.component';
+import { RaceShoesComponent } from './Components/Shoes/race-shoes/race-shoes.component';
+import { ViewProfileComponent } from './Components/User/view-profile/view-profile.component';
+import { ViewListNamesComponent } from './Components/SupplyLists/view-list-names/view-list-names.component';
 
 
 @NgModule({
@@ -44,7 +43,6 @@ import { ViewListNamesComponent } from './Components/view-list-names/view-list-n
     UpcomingRacesComponent,
     SupplyListComponent,
     ContactComponent,
-    RegisterComponent,
     ForgotPasswordComponent,
     ForgotUsernameComponent,
     SidebarNavComponent,
